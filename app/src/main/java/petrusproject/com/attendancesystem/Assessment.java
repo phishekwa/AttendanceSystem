276c24e3440f27156b91a39593d0e049aabc0132
Petrus Hishekwa <phishekwa@gmail.com>
2018-09-16 17:50:06 +0200

    Implemented a Spinner and a Number picker

--- app/src/main/java/petrusproject/com/attendancesystem/Assessment.java
+++ app/src/main/java/petrusproject/com/attendancesystem/Assessment.java
@@ -17,39 +17,50 @@ public class Assessment extends AppCompatActivity implements AdapterView.OnItemS
 
     //Declare variables
 
+    TextView txtViewModule, showDate, txtViewAssessment;
     Spinner categorySpinner, moduleSpinner;
     NumberPicker numberPickerAssessNumber, numberPickerAssessMarks;
-    ArrayAdapter<CharSequence> adapter;
+    ArrayAdapter<CharSequence> assessmentAdapter, moduleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment);
 
+
+        //Initialise Views
+        numberPickerAssessNumber = findViewById(R.id.assessmentNumId);
+        numberPickerAssessMarks = findViewById(R.id.assessmentTotalMarks);
+        moduleSpinner = findViewById(R.id.moduleSpinnerId);
+        showDate = findViewById(R.id.assessViewDateId);
+        categorySpinner = findViewById(R.id.categorySpinnerId);
+
         //Get current date
         Calendar dyt = Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.SHORT).format(dyt.getTime());
+        showDate.setText(currentDate);
 
-        TextView showDateAssess = findViewById(R.id.assessViewDateId);
-        showDateAssess.setText(currentDate);
+        //Setting a assessment spinner assessmentAdapter
+        assessmentAdapter = ArrayAdapter.createFromResource(this,R.array.category, R.layout.support_simple_spinner_dropdown_item);
+        assessmentAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(assessmentAdapter);
 
-        //Defining a spinner to select categories from.
-        categorySpinner = findViewById(R.id.categorySpinnerId);
-        adapter = ArrayAdapter.createFromResource(this,R.array.category, R.layout.support_simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        categorySpinner.setAdapter(adapter);
+        //Setting a module spinner assessmentAdapter
+        moduleAdapter = ArrayAdapter.createFromResource(this,R.array.modules, R.layout.support_simple_spinner_dropdown_item);
+        moduleAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        moduleSpinner.setAdapter(moduleAdapter);
 
         //Define the click listener method on item selected
-        int initialPosition = categorySpinner.getSelectedItemPosition();
-        categorySpinner.setSelection(initialPosition, false);
+        categorySpinner.setSelection(0, false);
+        moduleSpinner.setSelection(0, false);
+
+        moduleSpinner.setOnItemSelectedListener(this);
         categorySpinner.setOnItemSelectedListener(this);
 
         //set up a number pickers
-        numberPickerAssessNumber = findViewById(R.id.assessmentNumId);
         numberPickerAssessNumber.setMinValue(1);
         numberPickerAssessNumber.setMaxValue(10);
         numberPickerAssessNumber.getWrapSelectorWheel();
 
-        numberPickerAssessMarks = findViewById(R.id.assessmentTotalMarks);
         numberPickerAssessMarks.setMinValue(5);
         numberPickerAssessMarks.setMaxValue(100);
         numberPickerAssessMarks.getWrapSelectorWheel();
@@ -57,16 +68,37 @@ public class Assessment extends AppCompatActivity implements AdapterView.OnItemS
 
     }
 
+
+    //Set select listener to the Assessment Spinners
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String itemSelected = parent.getItemAtPosition(position).toString();
-        if(itemSelected.equals("Select Category"))
+        String assessmentItemSelected = parent.getItemAtPosition(position).toString();
+
+
+
+     /*   if(assessmentItemSelected.equals("Select Category Here"))
         {
-            return;
+            txtViewAssessment.setText("No Assessment");
+
         }
-        else
-            Toast.makeText(parent.getContext(),itemSelected,Toast.LENGTH_LONG).show();
+        else {
+            Toast.makeText(parent.getContext(),assessmentItemSelected,Toast.LENGTH_LONG).show();
+            //txtViewAssessment.setText(assessmentItemSelected);
+        }*/
+
+        String moduleItemSelected = parent.getItemAtPosition(position).toString();
+
+       /* if(moduleItemSelected.equals("Select Module"))
+        {
+            txtViewModule.getText();
+        }
+        else {
+            txtViewModule.setText(moduleItemSelected);
+        }*/
+       return;
     }
+
+
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
--- app/src/main/java/petrusproject/com/attendancesystem/Attendance.java
+++ app/src/main/java/petrusproject/com/attendancesystem/Attendance.java
@@ -1,25 +1,74 @@
 package petrusproject.com.attendancesystem;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.DateFormat;
-import java.text.Format;
 import java.util.Calendar;
 
-public class Attendance extends AppCompatActivity {
+public class Attendance extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    //Declaring variables
+    TextView showDate, txtShowModuleId;
+    EditText edItemSelected;
+    Spinner moduleSpinner;
+    ArrayAdapter<CharSequence> moduleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
 
+
+        //Initialise variables
+        showDate = findViewById(R.id.attendViewDateId);
+        moduleSpinner = findViewById(R.id.moduleSpinnerAttendId);
+
         //Get current date
         Calendar dyt = Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.SHORT).format(dyt.getTime());
+        showDate.setText(currentDate);
 
-        TextView showDateAttend = findViewById(R.id.attendViewDateId);
-        showDateAttend.setText(currentDate);
+
+        //Defining the adapter for a spinner
+        moduleAdapter = ArrayAdapter.createFromResource(this,R.array.modules,R.layout.support_simple_spinner_dropdown_item);
+        moduleAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        moduleSpinner.setAdapter(moduleAdapter);
+
+        moduleSpinner.setSelection(0, false); //don't add the first Item to the textView.
+
+        //Establish the onclick listener of the spinner
+        moduleSpinner.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String itemSelected = parent.getItemAtPosition(position).toString();
+        /*if (itemSelected.equals("Select Module"))
+        {
+            txtShowModuleId.setText("No Module");
+        }
+        else {
+            txtShowModuleId.setText(itemSelected);
+
+        }*/
+        return;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
